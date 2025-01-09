a2c3894c43eab7d684e8f0d17d4f73af392de429
Linus <khabibtime@rambler.ru>
2025-01-09 22:44:06 +0300

    1

--- app/user/routes.py
+++ app/user/routes.py
@@ -1,6 +1,10 @@
 from . import user
-from flask import request, render_template, session, redirect, url_for
+from db import db_connect
+
+from flask import request, render_template, redirect, url_for
 import uuid
+from datetime import datetime, timedelta
+
 
 @user.route("/")
 def index():
@@ -9,11 +13,24 @@ def index():
 @user.route("/submit", methods=["POST", "GET"])
 def submit():
     if request.method == "POST":
+        conn = db_connect()
+        cur = conn.cursor()
+
         unique_id = uuid.uuid1()
         note = request.form.get("note")
-        timer = request.form.get("timer")
+        timer = int(request.form.get("timer"))
+        date = datetime.now() + timedelta(minutes=timer)
         password = request.form.get("password")
+
+        cur.execute("""
+        INSERT INTO notes(unique_id, note, timer, password)
+        VALUES(%s, %s, %s, %s)""", (unique_id, note, date, password,))
+
+        conn.commit()
+        conn.close()
+
         return redirect(url_for('user.result', id=unique_id))
+    
     return render_template("index.html")
 
 @user.route("/password/<uuid:id>", methods=["GET", "POST"])
